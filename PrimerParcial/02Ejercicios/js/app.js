352ab6ee3c1a8b0c05a9e66703af2ee7ea0c3af4
andy-vac <avaciop2400@alumno.ipn.mx>
2026-09-24 16:52:04 -0600

    codigo del 24 de sep

--- PrimerParcial/02Ejercicios/js/app.js
+++ PrimerParcial/02Ejercicios/js/app.js
@@ -69,3 +69,65 @@ formularioTalleres.addEventListener('submit', (evento) => {
         salidaResultado.textContent = 'Selecciona una operación válida.';
     }
 });
+
+
+// Ejercicio de objeto
+
+const formObjeto = document.getElementById('form-objetivo');
+const resultadoObjeto = document.getElementById('resultado-objeto');
+
+formObjeto.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    //necesitamos construir el objeto del taller
+    const taller = {
+        nombre : document.getElementById('obj-nombre').value,
+        instructor : document.getElementById('obj-instructor').value,
+        cupo : Number(document.getElementById('obj-cupo').value),
+        inscritos : Number(document.getElementById('obj-indcritos').value)
+
+    };
+
+    const operacion = document.getElementById('operacion-objeto').value;
+
+    let resultado;
+
+    switch(operacion) {
+        case 'keys':
+            resultado = JSON.stringify(Object.keys(taller));
+            break;
+
+        case 'values':
+            const valores = Object.values(taller);
+
+            resultado = [valores.join("\n"),'',`tipo: ${typeof valores}`].join("\n");
+        break;
+
+        case 'entries':
+            const entradas = Object.entries(taller);
+
+            resultado = [entradas.map(([campo, valor]) => `${campo}: ${valor}`),'',`tipo: ${typeof entradas}`].join("\n");
+        break;
+
+        case 'stringify':
+            // Jimmy
+            const textoJson = JSON.stringify(taller, null, 2);
+            resultado = `${textoJson}\n \n tipo: ${typeof textoJson}`;
+            break;
+
+        case 'roundtrip':
+            // Jimmy
+            const textoJsons = JSON.stringify(taller, null, 2);
+            const objetoDeVuelta = JSON.parse(textoJsons);
+
+            resultado = [
+                textoJsons,
+                '',
+                `tipo:${typeof objetoDeVuelta}`,
+                objetoDeVuelta.nombre
+            ].join(`\n`);
+            break;
+    }
+    
+    resultadoObjeto.textContent = resultado;
+});
